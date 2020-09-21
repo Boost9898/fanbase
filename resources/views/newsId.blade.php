655df02dc2018124da4c1bd9b69ddfd7685172e8
Boost9898 <bls.place@gmail.com>
2020-09-21 13:57:21 +0200

    Changed the name 'image' to 'media' since the database changed the name of it

--- resources/views/newsId.blade.php
+++ resources/views/newsId.blade.php
@@ -42,7 +42,7 @@
                                         @if($item['id']==$id)
                                             <li>{{$item['title']}}</li>
                                             <li>{{$item['description']}}</li>
-                                            <img src="{{$item['media']}}">
+                                            <img alt="placeholder" src="{{$item['media']}}">
                                             <br>
                                         @endif
                                     @endforeach
