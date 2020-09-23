437961f49e540080575d7d0086868a0b2d14615f
Boost9898 <bls.place@gmail.com>
2020-09-23 20:48:48 +0200

    Added a vector logo in the most ghetto way possible, but it works for now.

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -19,7 +19,7 @@
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <img src="images/fanbaseLogo.svg">
+                <img src="https://raw.githubusercontent.com/Boost9898/fanbase/master/public/images/fanbaseLogo.svg">
             </div>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
