7c84798e11f4f1f2a172f95cf2db16b653f47bdc
Boost9898 <bls.place@gmail.com>
2020-09-25 11:03:45 +0200

    Installed Laravel Jetstream/Livewire

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('role_admin');
-            $table->boolean('role_verified');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
         });
     }
